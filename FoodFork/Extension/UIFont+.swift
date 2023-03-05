8c7e4accdf8884051a135ca8840c4c955e4e179f
Ivy <69495341+AtenB3f@users.noreply.github.com>
2023-03-05 23:03:20 +0900

    Feat(UI): 아이콘, 폰트 추가

---
+++ FoodFork/Extension/UIFont+.swift
@@ -0,0 +1,26 @@
+//
+//  UIFont+.swift
+//  FoodFork
+//
+//  Created by Ivy Moon on 2023/03/05.
+//
+
+import UIKit
+
+//class Font {
+extension UIFont {
+    // Body
+    static var fontCaption = UIFont(name: "NotoSansKR-Regular", size: 12)!
+    static var fontBody1 = UIFont(name: "NotoSansKR-Regular", size: 13)!
+    static var fontBody2 = UIFont(name: "NotoSansKR-Regular", size: 15)!
+    static var fontBody3 = UIFont(name: "NotoSansKR-Regular", size: 17)!
+    
+    // Subtitle
+    static var fontSubtitle1 = UIFont(name: "NotoSansKR-Regular", size: 18)!
+    static var fontSubtitle2 = UIFont(name: "NotoSansKR-Regular", size: 22)!
+    
+    // Header
+    static var fontHeader1 = UIFont(name: "NotoSansKR-Regular", size: 24)!
+    static var fontHeader2 = UIFont(name: "NotoSansKR-Regular", size: 28)!
+    static var fontHeader3 = UIFont(name: "NotoSansKR-Regular", size: 36)!
+}
