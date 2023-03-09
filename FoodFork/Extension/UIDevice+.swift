efa3371cd99a3c9d13cfa4f906bca89ff5d1bc24
Ivy <69495341+AtenB3f@users.noreply.github.com>
2023-03-09 13:47:44 +0900

    Feat(Library): 카카오맵 라이브러리 추가, bundle id 수정

---
+++ FoodFork/Extension/UIDevice+.swift
@@ -0,0 +1,26 @@
+//
+//  UIDevice+.swift
+//  FoodFork
+//
+//  Created by Ivy Moon on 2023/03/09.
+//
+
+import UIKit
+
+extension UIDevice {
+    // 노치 바텀 높이
+    var notchHeightBottom: CGFloat {
+        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        let window = windowScene?.windows.first
+        print(window?.safeAreaInsets.bottom)
+        return window?.safeAreaInsets.bottom ?? 0
+    }
+    
+    // 노치 탑 높이
+    var notchHeightTop: CGFloat {
+        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        let window = windowScene?.windows.first
+        
+        return window?.windowScene?.keyWindow?.safeAreaInsets.top ?? 0
+    }
+}
