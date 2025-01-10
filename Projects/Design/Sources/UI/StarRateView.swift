e5a1ed5c4120ea14ad9b7f8d0c09b5249d6e2ac3
atenb <atenb3f@gmail.com>
2025-01-10 11:19:55 +0900

    Refactor(AddReview): 별점 설정 UI 개선
    - 탭 제스처 시 소수점 없이 별점 설정되도록 수정
    - 드래그 제스처 시 별점 그라데이션 색상
    - 별점 0 표기 0 -> 0.0

--- Projects/Design/Sources/UI/StarRateView.swift
+++ Projects/Design/Sources/UI/StarRateView.swift
@@ -92,6 +92,7 @@ public class StarRateView: UIView {
         var value = Double(location.x / (stackView.frame.maxX - stackView.frame.minX) * CGFloat(total))
         value = value < 0 ? 0 : value
         value = value < total ? value : total
+        value = floor(value)
         
         for i in 0..<stackView.arrangedSubviews.count {
             let star = stackView.arrangedSubviews[i]
@@ -103,14 +104,38 @@ public class StarRateView: UIView {
     public func panHandler(_ sender: UIPanGestureRecognizer) {
         let point = sender.translation(in: self)
         let location = sender.location(in: self)
-        var value = Int((point.x + location.x) / (stackView.frame.maxX - stackView.frame.minX) * CGFloat(total))
+        var value = Double((point.x + location.x) / (stackView.frame.maxX - stackView.frame.minX) * CGFloat(total))
         value = value < 0 ? 0 : value
-        value = value < Int(total) ? value : Int(total)
-        
+        value = value < total ? value : total
+        let starValue = Int(value)
+
         for i in 0..<stackView.arrangedSubviews.count {
             let star = stackView.arrangedSubviews[i]
-            stackView.arrangedSubviews[i].tintColor = star.tag < value ? enableColor : disableColor
+            var color = disableColor
+            if star.tag <= starValue {
+                color = enableColor
+                if value != Double(starValue) && starValue == i {
+                    var rDis: CGFloat = 0
+                    var gDis: CGFloat = 0
+                    var bDis: CGFloat = 0
+                    var rEn: CGFloat = 0
+                    var gEn: CGFloat = 0
+                    var bEn: CGFloat = 0
+
+                    disableColor.getRed(&rDis, green: &gDis, blue: &bDis, alpha: nil)
+                    enableColor.getRed(&rEn, green: &gEn, blue: &bEn, alpha: nil)
+
+                    let red = rDis - abs(rEn - rDis) * (value - Double(starValue))
+                    let green = gDis - abs(gEn - gDis) * (value - Double(starValue))
+                    let blue = bDis - abs(bEn - bDis) * (value - Double(starValue))
+                    color = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+                }
+            } else {
+                color = disableColor
+            }
+            
+            stackView.arrangedSubviews[i].tintColor = color
         }
-        setRate?(Double(value))
+        setRate?(value)
     }
 }
--- Projects/FoodFork/Sources/Extension/Double+.swift
+++ Projects/FoodFork/Sources/Extension/Double+.swift
@@ -14,16 +14,6 @@ extension Double {
      : 소수점 첫재 자리까지 보여준다.
      */
     var toStarRateString: String {
-        if self == 0.0 { return "0" }
         return String(format: "%.1f", self)
-//        let formatNumber = floor(self * 10)
-//        let number = formatNumber / 10
-//        let remainNumber = number.truncatingRemainder(dividingBy: 1.0)
-//
-//        if remainNumber == 0.0 {
-//            return "\(Int(number))"
-//        } else {
-//            return String(format: "%.1f", number)
-//        }
     }
 }
